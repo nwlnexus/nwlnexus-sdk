f0bea8015ccaf83b6192391d0f7639001eb28ed0
Nigel Williams-Lucas <nigel.williamslucas@gmail.com>
2023-11-05 15:39:55 -0500

    Trying Next.JS

---
+++ apps/helios/.prettierrc.mjs
@@ -0,0 +1,24 @@
+const config = {
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 120,
+  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx'],
+  importOrder: [
+    '<BUILTIN_MODULES>',
+    'react',
+    '@remix-run/react',
+    '@remix-run/.*',
+    '<THIRD_PARTY_MODULES>',
+    '<TYPES>',
+    '<TYPES>^[.]',
+    '^[.]',
+    '^(?!.*[.]css$)[./].*$',
+    '.css$'
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0'
+};
+
+export default config;
--- apps/helios/tailwind.config.ts
+++ apps/helios/tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    '../../node_modules/daisyui/dist/**/*.js',
+    '../../node_modules/react-daisyui/dist/**/*.js'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [],
-}
-export default config
+  daisyui: {
+    themes: ['light', 'dark', 'synthwave', 'corporate', 'black', 'dracula', 'night', 'valentine', 'pastel', 'cmyk'],
+    darkTheme: 'dark',
+    base: true,
+    styled: true,
+    utils: true,
+    log: false
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('daisyui')]
+};
+export default config;
