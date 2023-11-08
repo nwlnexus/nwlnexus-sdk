8c5b28043e211fce724a9536c0e36612bb9af1b6
Nigel Williams-Lucas <nigel.williamslucas@gmail.com>
2023-11-08 07:55:16 -0500

    Styling the navbar

--- apps/helios/src/app/(app)/page.tsx
+++ apps/helios/src/app/(app)/page.tsx
@@ -3,19 +3,28 @@ import { Hero, WindowMockup } from 'react-daisyui';
 export default function LandingView() {
   return (
     <main className="flex min-h-screen justify-center p-24">
-      <Hero className="h-1/2">
-        <Hero.Content>
+      <div className="flex min-h-[550vh] max-w-[100vw] flex-col items-center justify-start xl:flex-row xl:items-start xl:justify-between">
+        <div className="shrink xl:w-1/2">
+          <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center px-2 py-10 text-center xl:justify-start xl:pl-10 xl:pr-0 xl:text-left">
+            <div>
+              <h1 className="font-title text-center text-[clamp(2rem,6vw,4.2rem)] font-black leading-[1.1] xl:text-left">
+                The system administration application.
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="invisible sticky bottom-4 flex w-[calc(100%-2rem)] shrink duration-700 xl:visible xl:-right-32 xl:bottom-auto xl:top-16 xl:w-auto xl:!transform-none xl:overflow-hidden xl:overflow-y-clip xl:bg-transparent xl:pb-16 xl:pt-16">
           <WindowMockup
             border
             borderColor="accent"
             frameColor="base-100"
             backgroundColor="base-300"
-            className="h-1/2 w-[400px]"
+            className="h-1/2 w-full bg-base-200/90 backdrop-blur"
           >
             <div className="flex w-auto justify-center px-4 py-16">Hello!</div>
           </WindowMockup>
-        </Hero.Content>
-      </Hero>
+        </div>
+      </div>
     </main>
   );
 }
--- apps/helios/src/components/ui/NavBar.tsx
+++ apps/helios/src/components/ui/NavBar.tsx
@@ -1,7 +1,7 @@
-import { Button, Join, Navbar } from 'react-daisyui';
+import { Button, Join, Navbar, Tooltip } from 'react-daisyui';
 import ThemeToggle from '@/components/ui/ThemeToggle';
 import useScroll from '@/hooks/use-scroll';
-import Link from 'next/link';
+import clsx from 'clsx';
 
 type NavBarProps = {
   toggleVisible: () => void;
@@ -12,32 +12,37 @@ export default function NavBar({ toggleVisible }: NavBarProps) {
   return (
     <>
       <Navbar
-        className={`fixed top-0 z-30 flex w-full justify-center transition-all ${
-          scrolled ? 'border-b border-gray-200 bg-white/50 backdrop-blur-xl' : 'bg-white/0'
-        }`}
+        className={clsx(
+          'sticky top-0 z-30 h-16 justify-center bg-base-100 bg-opacity-90 text-base-content backdrop-blur transition-all duration-100 [transform:translate3d(0,0,0)]',
+          { 'shadow-sm': scrolled }
+        )}
       >
         <div className="flex-none lg:hidden">
-          <Button shape="square" color="ghost" onClick={toggleVisible}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-6 w-6 stroke-current"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-          </Button>
+          <Tooltip message="Menu" position="bottom">
+            <Button shape="square" color="ghost" onClick={toggleVisible}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+            </Button>
+          </Tooltip>
         </div>
-        <div className="flex-1">
-          <svg height="30" width="100" xmlnsXlink="http://www.w3.org/1999/xlink">
-            <Link href="/" className="font-display flex items-center text-2xl">
-              <text x="0" y="30" className="dark:text-purple-400">
-                HELIOS
-              </text>
-            </Link>
-          </svg>
+        <div className="flex flex-1 md:gap-1 lg:gap-2">
+          <div className="flex items-center gap-2">
+            <Button tag="a" color="ghost" aria-label="Homepage" href="/" className="flex-0 gap-1 px-2 md:gap-2">
+              <div className="font-title inline-flex text-lg md:text-2xl">
+                <span className="lowercase">helios</span>
+                <span className="uppercase text-accent">UI</span>
+              </div>
+            </Button>
+            <div className="font-mono text-xs">{process.env.NEXT_PUBLIC_APP_VERSION}</div>
+          </div>
         </div>
-        <Join className="flex-none gap-2">
+        <Join className="flex-none gap-x-2">
           <ThemeToggle />
           <Button size="md" color="accent" variant="outline" aria-label="sign in">
             Sign In
