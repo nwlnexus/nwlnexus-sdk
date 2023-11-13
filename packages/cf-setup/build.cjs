0dc6e1d527b5a90f89451e1c195461fce6c5aee9
Nigel Williams-Lucas <yesniah@gmail.com>
2023-11-13 17:30:40 -0500

    CF Setup

--- packages/cf-setup/build.cjs
+++ packages/cf-setup/build.cjs
@@ -1,16 +1,19 @@
-const { build } = require("esbuild");
-const { dependencies } = require('./package.json')
+const { build } = require('esbuild');
+const { dependencies } = require('./package.json');
 
 const sharedConfig = {
-  entryPoints: ["src/index.ts"],
+  entryPoints: ['src/index.ts'],
   bundle: true,
   minify: true,
-  external: Object.keys(dependencies),
+  external: Object.keys(dependencies).concat()
 };
 
 build({
   ...sharedConfig,
   platform: 'node', // for CJS
-  outfile: "dist/index.js",
-  format: "esm"
+  outfile: 'dist/index.js',
+  format: 'esm'
+}).catch((err) => {
+  console.log(err);
+  process.exit(1);
 });
