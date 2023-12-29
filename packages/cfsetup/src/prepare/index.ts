d63bec20e585ba2536fcb07ecd6c6f7ec58f69d7
Nigel Williams-Lucas <nigel.williamslucas@gmail.com>
2023-12-29 11:05:10 -0500

    Refactoring cfsetup prepare command

--- packages/cfsetup/src/prepare/index.ts
+++ packages/cfsetup/src/prepare/index.ts
@@ -1,12 +1,10 @@
 import path from 'node:path';
 import process from 'node:process';
 
-import { yellow } from '../colors';
 import { withConfig } from '../config';
 import { STORAGE_MEDIUM } from '../constants';
-import { resetCFAssets } from '../reset';
 import { CommonYargsArgv, StrictYargsOptionsToInterface } from '../root-arguments';
-import { setupDB, setupKV, setupR2 } from './storage';
+import { handleD1, handleKV, handleR2 } from './utils';
 
 export function prepareOptions(args: CommonYargsArgv) {
   return args
@@ -57,64 +55,23 @@ export function prepareOptions(args: CommonYargsArgv) {
 
 export const prepareHandler = withConfig<StrictYargsOptionsToInterface<typeof prepareOptions>>(
   async ({ config, schemaDir, persistTo, reset, storage }) => {
-    const handleD1 = async () => {
-      if ('d1_databases' in config) {
-        for (const database of config.d1_databases as Array<Record<string, string>>) {
-          const mDir = typeof database['migrations_dir'] == 'undefined' ? 'migrations/' : database['migrations_dir'];
-          if (reset) {
-            resetCFAssets(mDir);
-            resetCFAssets(path.join(persistTo, 'v3/d1'));
-          }
-          await setupDB(database['binding'], schemaDir, mDir, persistTo);
-        }
-      } else {
-        console.info(yellow(`Skipping D1 setup. --d1 not specified or no d1_databases defined in wrangler.toml`));
-      }
-    };
-
-    const handleKV = async () => {
-      if ('kv_namespaces' in config) {
-        for (const kv of config.kv_namespaces as Array<Record<string, string>>) {
-          if (reset) {
-            resetCFAssets(path.join(persistTo, 'v3/kv'));
-          }
-          await setupKV(kv['binding']);
-        }
-      } else {
-        console.info(yellow(`Skipping KV setup. --kv not specified or no kv_namespaces defined in wrangler.toml`));
-      }
-    };
-
-    const handleR2 = async () => {
-      if ('r2_namespaces' in config) {
-        for (const r2 of config.r2_namespaces as Array<Record<string, string>>) {
-          if (reset) {
-            resetCFAssets(path.join(persistTo, 'v3/r2'));
-          }
-          await setupR2(r2['binding']);
-        }
-      } else {
-        console.info(yellow(`Skipping R2 setup. --r2 not specified or no r2_namespaces defined in wrangler.toml`));
-      }
-    };
-
     switch (storage) {
       case 'all': {
-        await handleD1();
-        await handleKV();
-        await handleR2();
+        await handleD1(config.d1_databases, schemaDir, persistTo, reset);
+        await handleKV(config.kv_namespaces, persistTo, reset);
+        await handleR2(config.r2_buckets, persistTo, reset);
         break;
       }
       case 'd1': {
-        await handleD1();
+        await handleD1(config.d1_databases, schemaDir, persistTo, reset);
         break;
       }
       case 'kv': {
-        await handleKV();
+        await handleKV(config.kv_namespaces, persistTo, reset);
         break;
       }
       case 'r2': {
-        await handleR2();
+        await handleR2(config.r2_buckets, persistTo, reset);
         break;
       }
     }
--- packages/cfsetup/src/prepare/utils.ts
+++ packages/cfsetup/src/prepare/utils.ts
@@ -1,6 +1,12 @@
 import type { Config } from '../config';
 import type { Database } from './types';
 
+import child_process from 'node:child_process';
+import path from 'node:path';
+import chalk from 'chalk';
+
+import { yellow } from '../colors';
+import { resetCFAssets } from '../reset';
 import { DEFAULT_MIGRATION_PATH, DEFAULT_MIGRATION_TABLE } from './constants';
 
 export function getDatabaseInfoFromConfig(config: Config, name: string): Database | null {
@@ -19,3 +25,65 @@ export function getDatabaseInfoFromConfig(config: Config, name: string): Databas
   }
   return null;
 }
+
+export const handleD1 = async (
+  d1_databases: Config['d1_databases'],
+  schemaDir: string,
+  persistTo: string,
+  reset?: boolean
+) => {
+  if (d1_databases && d1_databases.length > 0) {
+    for (const database of d1_databases) {
+      const mDir = database.migrations_dir ?? DEFAULT_MIGRATION_PATH;
+      if (reset) {
+        resetCFAssets(mDir);
+        resetCFAssets(path.join(persistTo, 'v3', 'd1'));
+      }
+
+      console.info(chalk.blue(`Preparing to setup D1 Database: ${database.binding}`));
+      // const schema = path.normalize(schemaDir);
+      // const outDir = path.normalize(`${migrationsDir}`);
+      console.info(chalk.blue(`Preparing generate SQL...`));
+      const cmd = `drizzle-kit generate:sqlite --schema=${schemaDir} --out=${mDir}`;
+      child_process.execSync(cmd, {
+        stdio: 'inherit',
+        encoding: 'utf8'
+      });
+      console.info(chalk.blue(`Preparing apply SQL...`));
+      const wranglerCmd = `NO_D1_WARNING=true wrangler d1 migrations apply ${database.binding} --local --persist-to=${persistTo}`;
+      child_process.execSync(wranglerCmd, {
+        stdio: 'inherit',
+        encoding: 'utf8'
+      });
+      console.info(chalk.blue(`Completed setup of D1 Database: ${database.binding}`));
+    }
+  } else {
+    console.info(yellow(`Skipping D1 setup. --d1 not specified or no d1_databases defined in wrangler.toml`));
+  }
+};
+
+export const handleKV = async (kv_namespaces: Config['kv_namespaces'], persistTo: string, reset?: boolean) => {
+  if (kv_namespaces && kv_namespaces.length > 0) {
+    for (const kv of kv_namespaces) {
+      if (reset) {
+        resetCFAssets(path.join(persistTo, 'v3', 'kv'));
+      }
+      console.info(chalk.blue(`Preparing to setup KV: ${kv.binding}`));
+    }
+  } else {
+    console.info(yellow(`Skipping KV setup. --kv not specified or no kv_namespaces defined in wrangler.toml`));
+  }
+};
+
+export const handleR2 = async (r2_buckets: Config['r2_buckets'], persistTo: string, reset?: boolean) => {
+  if (r2_buckets && r2_buckets.length > 0) {
+    for (const r2_bucket of r2_buckets) {
+      if (reset) {
+        resetCFAssets(path.join(persistTo, 'v3', 'r2'));
+      }
+      console.info(chalk.blue(`Preparing to setup R2: ${r2_bucket.binding}`));
+    }
+  } else {
+    console.info(yellow(`Skipping R2 setup. --r2 not specified or no r2_buckets defined in wrangler.toml`));
+  }
+};
